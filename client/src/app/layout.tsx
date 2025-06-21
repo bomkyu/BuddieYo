8fe3affbb29451634b228a6e1545858bf06c284b
bomkyu <a01090762806@gmail.com>
2025-06-21 18:39:31 +0900

    fix : layout.tsx pretendard font 추가

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,15 +1,31 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import localFont from 'next/font/local';
+
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const predendard = localFont({
+  src: [
+    {
+      path: './fonts/Pretendard-Medium.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Pretendard-Regular.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Pretendard-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Pretendard-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
 });
 
 export const metadata: Metadata = {
@@ -23,12 +39,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang='en'>
+      <body className={predendard.className}>{children}</body>
     </html>
   );
 }
