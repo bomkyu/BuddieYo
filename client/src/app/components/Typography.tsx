fd1f196a43a9eac428fa3df932fad29b5a203640
bomkyu <a01090762806@gmail.com>
2025-07-09 17:45:56 +0900

    chore : size 추가 및 태그 지정 추가

--- client/src/app/components/Typography.tsx
+++ client/src/app/components/Typography.tsx
@@ -3,8 +3,20 @@ import { JSX } from 'react';
 type Typography = {
   children: React.ReactNode;
   weight?: 'medium' | 'normal' | 'semibold' | 'bold';
-  size?: 'sm' | 'base' | 'lg' | 'xl' | '2xl';
+  size?:
+    | 'sm'
+    | 'base'
+    | 'lg'
+    | 'xl'
+    | '2xl'
+    | '3xl'
+    | '4xl'
+    | '5xl'
+    | '6xl'
+    | '7xl'
+    | '8xl';
   as?: keyof JSX.IntrinsicElements; // 'p' | 'span' | 'h1' 등 태그 지정
+  className?: string;
 };
 
 const weightClassMap = {
@@ -20,6 +32,12 @@ const sizeClassMap = {
   lg: 'text-lg',
   xl: 'text-xl',
   '2xl': 'text-2xl',
+  '3xl': 'text-3xl',
+  '4xl': 'text-4xl',
+  '5xl': 'text-5xl',
+  '6xl': 'text-6xl',
+  '7xl': 'text-7xl',
+  '8xl': 'text-8xl',
 } as const;
 
 const Typography = ({
@@ -27,12 +45,13 @@ const Typography = ({
   weight = 'medium',
   size = 'base',
   as: Component = 'p',
+  className,
 }: Typography) => {
   const fontWeightClass = weightClassMap[weight];
   const sizeClass = sizeClassMap[size];
 
   return (
-    <Component className={`${fontWeightClass} ${sizeClass}`}>
+    <Component className={`${fontWeightClass} ${sizeClass} ${className}`}>
       {children}
     </Component>
   );
